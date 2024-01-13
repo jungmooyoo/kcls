41229fe735ad104b43af59a3566fb0fc00d8b290
mcr01 <mcr01@192.168.219.194>
2024-01-13 15:58:02 +0900

    forth method

--- src/aspect/AspectTest.java
+++ src/aspect/AspectTest.java
@@ -5,7 +5,7 @@ public class AspectTest {
 	
 	private void publicTarget() { 
 		if ("test".equals("test")) {
-		    System.out.println("publicTarget Startqqqqqq!!");
+		    System.out.println("publicTarget Startqqqqqq111111111");
 		}else {
 			System.out.println("publicTarget End!!"); 
 		}
